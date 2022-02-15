fc54918139847fb85402613bb8cd7308d10f0f24
Pablo Zuta <pablo.zuta.h@gmail.com>
2022-02-15 16:13:52 -0400

    creacion responsive navbar
    se modifica el codigo de app.js
    se termina la creacion de responsive navbar modificando el archivo principal css
    y tambien el archivo index.html dejandolo solo con el navbar

--- js/app.js
+++ js/app.js
@@ -1,42 +1,6 @@
-//aca empezamos el aprendizaje de JavaScript
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-console.log("Hola Terricolas!!");
-
-//aca declaramos una variable
-//las variables no pueden tener un guion en el nombre
-let clowns = "We are the Clowns from Outter Space!!!";
-console.log("");
-console.log(clowns);
-
-//aca declaramos un par de arrays
-let nombres = ["John Coltrane", "Taurus", "Mantra"];
-let nombres2 = ["Miles Davis", "So What!", "Man on the Moon"];
-//hacemos un console.log de los array completos
-console.log(nombres);
-console.log(nombres2);
-//y aca un elemento dentro del array
-console.log(nombres[1]);
-console.log(nombres2[0]);
-console.log("");
-
-//aca hacemos un par de operaciones matematicas
-suma = (234 + 3524) ;
-console.log(suma);
-suma2 = (23434 + 353424) ;
-console.log(suma2);
-console.log("");
-
-//aca declaramos variables constantes
-const disco = "Blue Train";
-console.log(disco);
-//aca declaramos variables constantes
-const disco2 = "Kind of Blue";
-console.log(disco2);
-
-//probamos usar el metodo de asignacion var
-var queso = "Yummi";
-console.log(queso);
-
-//aca una variable numerica
-let bolaños = 2666;
-console.log(bolaños);
+toggleButton.addEventListener('click',() => {
+    navbarLinks.classList.toggle('active')
+} )
